82ca4f0e10cba47858cbf263220d2696d4e43d14
MacStudent <macstudent@126-iMac21.local>
2019-01-30 20:33:56 -0500

    third commit

--- javasample/src/javasample/Javasample.java
+++ javasample/src/javasample/Javasample.java
@@ -17,12 +17,22 @@ public class Javasample {
     public static void main(String[] args) {
         // TODO code application logic here
         student s1=new student();
+        student s2=new student();
+        
         s1.setSname("gurvinder");
         s1.setResult("pass");
         s1.setRollnumber(12);
         System.out.println(s1.getSname());
         System.out.println(s1.getResult());
         System.out.println(s1.getRollnumber());
+        
+        
+        s2.setSname("mangat");
+        s2.setResult("pass");
+        s2.setRollnumber(134);
+        System.out.println(s2.getSname());
+        System.out.println(s2.getResult());
+        System.out.println(s2.getRollnumber());
     }
     
 }
